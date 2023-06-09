76b66df5e5d4ac4c5e8b4709607b46a262d49b4f
tanquan <2685303161@qq.com>
2023-06-09 19:15:53 +0800

    add 添加登录页面

--- src/pages/user/Login.vue
+++ src/pages/user/Login.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <h3>Login</h3>
+  <div id="login-bg">
+    <div class="login-form">
+      <div class="login-form-left">
+        <div>Welcome</div>
+        <div>后台管理系统</div>
+      </div>
+      <div class="login-form-right">
+        <a-input placeholder="Input account" size="large" class="login-form-input">
+          <a-icon slot="prefix" type="user" />
+        </a-input>
+        <a-input-password placeholder="Input password" size="large" class="login-form-input">
+          <a-icon slot="prefix" type="lock" />
+        </a-input-password>
+        <a-button type="primary" shape="round" size="large" class="login-form-button">
+          登录
+        </a-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +24,45 @@
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+#login-bg {
+  display: flex;
+  background: no-repeat url(../../assets/login_bg.jpg);
+  background-size: 100% 100%;
+  background-attachment: fixed;
+  height: 680px;
+}
+.login-form {
+  display: flex;
+  margin: auto;
+  width: 800px;
+  height: 400px;
+  border: 1px solid rgb(77, 88, 119);
+}
+.login-form-left {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 40%;
+  height: 100%;
+  opacity: 0.4;
+  background-color: #1890ff;
+}
+.login-form-left div {
+  font-size: 20px;
+}
+.login-form-right {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+.login-form-input {
+  width: 80%;
+  margin: 10px auto;
+}
+.login-form-button {
+  width: 75%;
+  margin: 20px auto;
+}
+</style>
