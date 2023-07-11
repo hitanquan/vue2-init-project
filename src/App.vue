d824f92a36e0183b0075f96bd881ba14d75a46ba
tanquan <2685303161@qq.com>
2023-07-11 17:33:07 +0800

    update 调整字体，即默认跟随电脑字体

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,6 @@ export default {
 
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   margin: 0;
   padding: 0;
 }
