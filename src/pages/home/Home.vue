1ba50bd27659d0817e52062bd567c87ac25d0a5f
tanquan <2685303161@qq.com>
2023-07-13 15:29:45 +0800

    update 动态面包屑

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -5,8 +5,9 @@
       <layout-head-nav-bar />
       <a-layout-content style="margin: 0 16px">
         <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>User</a-breadcrumb-item>
-          <a-breadcrumb-item>Bill</a-breadcrumb-item>
+          <a-breadcrumb-item v-for="(item, index) in this.$route.matched" :key="index">
+            {{ item.meta.title }}
+          </a-breadcrumb-item>
         </a-breadcrumb>
         <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
           <router-view />
